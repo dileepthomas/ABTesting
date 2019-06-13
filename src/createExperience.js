77033a656b6b9f4d44ad0251d3352de9fdf137ae
INE12362972 <vinay.kulkarni1@tesco.com>
2019-06-13 15:31:07 +0530

    Vinay | form creation

--- src/createExperience.js
+++ src/createExperience.js
@@ -1,12 +1,113 @@
 
 import React from 'react';
+import './createExperience.css';
 
-const createExperience = () => {
-    return (
-        <div>
-            <h1> Create Experience</h1>
-				</div>
-  )
+export class createExperience extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			formObj: {}
+
+		};
+		
+	}
+
+	formOnChangeHandler = (e) => {
+		let formKeys = Object.keys(this.state.formObj)
+			
+		 if (formKeys.includes(e.target.name)) {
+			let copy = { ...this.state.formObj }
+			copy[e.target.name] = e.target.value
+			this.setState({ formObj: copy })
+		}
+
+		let copy = { ...this.state.formObj }
+			copy[e.target.name] = e.target.value
+			this.setState({ formObj: copy })
+
+	}
+
+	_handleSubmit = e => {
+		
+		e.preventDefault();
+		console.log(this.state.formObj)
+	}
+	render() {
+		return (
+			<div className="container" >
+				<form onChange={this.formOnChangeHandler}
+					onSubmit={this._handleSubmit} >
+					<div className="row">
+						<div className="col-25">
+							<label htmlFor="fname">Experiment name</label>
+						</div>
+						<div className="col-75">
+							<input type="text" id="expName" name="experimentName" placeholder="Experiment name.." />
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-25">
+							<label htmlFor="attributes">Attributes</label>
+						</div>
+						<div className="col-25">
+							<label htmlFor="lname">Name</label>
+
+							<div className="col-75">
+								<input type="text" id="attrName" name="attrName"  />
+							</div>
+						</div>
+
+						<div className="col-25">
+							<label htmlFor="lname">Default value</label>
+
+							<div className="col-75">
+								<input type="text" id="defaultValue" name="defaultValue"  />
+							</div>
+						</div>
+
+					</div>
+					<div className="row">
+						<div className="col-25">
+							<label htmlFor="percentage">Percentage</label>
+						</div>
+						<div className="col-25">
+							<input type="text" id="percentage" name="percentage"  />
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-25">
+							<label htmlFor="stickyness">Stickyness</label>
+						</div>
+						<div className="col-25">
+							<input type="text" id="stickyness" name="stickyness"  />
+						</div>
+						<div className="col-25">
+							<label htmlFor="stickyness">minutes</label>
+						</div>
+					</div>
+
+					<div className="row">
+						<div className="col-25">
+							<label htmlFor="stickyness">Experiment Duration</label>
+						</div>
+						<div className="col-25">
+							<input type="text" id="duration" name="duration"  />
+						</div>
+						<div className="col-25">
+							<label htmlFor="stickyness">minutes</label>
+						</div>
+					</div>
+
+					<div className="row">
+						<input type="submit" value="Submit"  />
+					</div>
+				</form>
+			</div>
+
+		)
+
+
+	}
 }
 
 export default createExperience;

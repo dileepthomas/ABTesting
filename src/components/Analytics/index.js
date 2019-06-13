e51d2f229a98a4aeda719f72a42aa9576e9a4cb6
Dileep Thomas <dileep.thomas@tesco.com>
2019-06-13 22:29:56 +0530

    Fixed Charts Css

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -3,15 +3,20 @@ import Constants from '../../constants/constant';
 import PieChart from '../PieChart';
 import BarChart from '../BarChart';
 import LineChart from '../LineChart';
+import './style.css';
 
 class Analytics extends Component {
   render() {
     const { pieChart, barChart, lineChart } = Constants;
     return (
-      <div>
-        <PieChart configData={pieChart} />
-        <BarChart configData={barChart} />
-        <LineChart configData={lineChart} />{' '}
+      <div className="charts-container">
+        <div className="first-section-container">
+          <LineChart configData={lineChart} />
+          <PieChart configData={pieChart} />
+        </div>
+        <div className="second-section-container">
+          <BarChart configData={barChart} />
+        </div>
       </div>
     );
   }

f4a93b3f4e5b868a8d13c90c3a79b3b87a3c997e
Vikas SC <vikas.channigaraya@tesco.com>
2019-06-13 15:08:16 +0530

    CSS changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Card from './components/Card/Card';
 
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="container-fluid mt-3">
-        <Card />
+      <div className="row abcard">
+          <Card name="Experiment" cardcolor="gradient-1"/>
+          <Card name="Anaysis" cardcolor="gradient-2"/>
       </div>
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,17 @@
 import React from 'react';
+import "./Card.css";
 
-function Card() {
-
+function Card(props) {
+    console.log(props.name);
     return (
-        <div className="row">
-            <div className="col-lg-3 col-sm-6">
-                <div className="card gradient-1">
-                <i className="fas fa-flask"></i>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+        <div className="col-lg-3 col-sm-6">
+            <div className={`card ${props.cardcolor}`}>
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
                 </div>
             </div>
         </div>
+
     );
 }
 

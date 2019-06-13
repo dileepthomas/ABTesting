701cab099a35515a798455ccef18462907056716
Vikas SC <vikas.channigaraya@tesco.com>
2019-06-13 16:31:43 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Card from './components/Card/Card';
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="appwidth">
       <Header />
       <div className="row abcard">
-          <Card name="Experiment" cardcolor="gradient-1"/>
-          <Card name="Anaysis" cardcolor="gradient-2"/>
+        <Card name="Experiment" cardcolor="gradient-1" />
+        <Card name="Anaysis" cardcolor="gradient-2" />
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import "./Footer.css";
+function Footer() {
+    return (
+        <footer className="footer text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="menu-footer-menu-container"><ul id="footer-menu" className=" footer-menu"><li id="menu-item-387" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-387"><a href="https://themefisher.com/license/">License</a></li>
+                            <li id="menu-item-395" className="menu-item menu-item-type-post_type menu-item-object-page current_page_parent menu-item-395"><a href="https://themefisher.com/blog/">Blog</a></li>
+                            <li id="menu-item-388" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-388"><a href="https://themefisher.com/contact/">Contact</a></li>
+                            <li id="menu-item-78795" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-78795"><a href="https://themefisher.com/about/">About</a></li>
+                            <li id="menu-item-243506" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-243506"><a href="https://themefisher.com/privacy-policy/">Privacy Policy</a></li>
+                        </ul></div>                <ul className="social-media-icons">
+                            <li>
+                                <a className="fb" href="https://www.facebook.com/themefisher">
+                                    <i className="ti-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a className="twitter" href="https://twitter.com/themefisher">
+                                    <i className="ti-twitter-alt"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a className="pinterest" href="https://www.pinterest.com/themefisher/">
+                                    <i className="ti-pinterest-alt"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a className="dribbble" href="https://www.dribbble.com/themefisher/">
+                                    <i className="ti-dribbble"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a className="dribbble" href="https://www.github.com/themefisher/">
+                                    <i className="ti-github"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <div className="copyright-text">
+                            <p>Copyright © 2019 Tesco</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+export default Footer;

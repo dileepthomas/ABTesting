5ed44691987c3709d6cb5a396da98fde53eccb69
INE12362972 <vinay.kulkarni1@tesco.com>
2019-06-13 21:20:20 +0530

    Vinay | create form changes

--- src/attributeForm.js
+++ src/attributeForm.js
@@ -2,28 +2,34 @@ import React from 'react';
 import './createExperience.css';
 
 const attributeForm = (props) => {
-	console.log('TRESSSS',props.counter);
-	 
 
 return(
 	Array.from({length:props.counter},(item,index)=>
 	<div>
-    <div className="col-25">
+    				<div className="col-25">
 							<label htmlFor="lname">Name</label>
 							<div className="col-75">
-								<input type="text" id="attrName" name="attrName"  />
+								<input type="text" id="attrName" name={`attrName-${index}`} />
 							</div>
 						</div>
 
 						<div className="col-25">
 							<label htmlFor="lname">Default value</label>
-
 							<div className="col-75">
-								<input type="text" id="defaultValue" name="defaultValue"  />
+								<input type="text" id="defaultValue" name={`defaultValue-${index}`}  />
 							</div>
-							<button className="addIcon" onClick={props._updateCounter}>+</button>
 						</div>
-					
+						{
+							console.log(index, props.counter)
+						}
+						{
+						(index+1 === props.counter  ) ? 
+									<button className="addIcon" onClick={props._updateCounter}>+</button>
+								:
+								null
+								
+						}	
+						
 				</div>
 )
 )
--- src/createExperience.js
+++ src/createExperience.js
@@ -11,31 +11,31 @@ export class createExperience extends React.Component {
 			counter: 1
 
 		};
-		
+
 	}
 
 	formOnChangeHandler = (e) => {
 		let formKeys = Object.keys(this.state.formObj)
-			
-		 if (formKeys.includes(e.target.name)) {
+
+		if (formKeys.includes(e.target.name)) {
 			let copy = { ...this.state.formObj }
 			copy[e.target.name] = e.target.value
 			this.setState({ formObj: copy })
 		}
 
 		let copy = { ...this.state.formObj }
-			copy[e.target.name] = e.target.value
-			this.setState({ formObj: copy })
+		copy[e.target.name] = e.target.value
+		this.setState({ formObj: copy })
 
 	}
 
 	_handleSubmit = e => {
 
 		e.preventDefault();
-		console.log(this.state.formObj)
+		localStorage.setItem('formValues', JSON.stringify(this.state.formObj))
 	}
-	_updateCounter = () =>{
-		this.setState({counter: this.state.counter+1},()=>{});
+	_updateCounter = () => {
+		this.setState({ counter: this.state.counter + 1 }, () => { });
 	}
 	render() {
 		return (
@@ -46,26 +46,25 @@ export class createExperience extends React.Component {
 						<div className="col-25">
 							<label htmlFor="fname">Experiment name</label>
 						</div>
-						<div className="col-75">
+						<div className="col-50">
 							<input type="text" id="expName" name="experimentName" placeholder="Experiment name.." />
 						</div>
 					</div>
-					<div className="row">
+					<div className="row a">
 						<div className="col-25">
 							<label htmlFor="attributes">Attributes</label>
 						</div>
-						
-					
-						<AttributeForm _updateCounter = { this._updateCounter} counter={this.state.counter} />
-								
+
+							<AttributeForm _updateCounter={this._updateCounter} counter={this.state.counter} />
 
 					</div>
-					<div className="row">
+
+					<div className="row disPercentage">
 						<div className="col-25">
 							<label htmlFor="percentage">Percentage</label>
 						</div>
 						<div className="col-25">
-							<input type="text" id="percentage" name="percentage"  />
+							<input type="text" id="percentage" name="percentage" />
 						</div>
 					</div>
 					<div className="row">
@@ -73,30 +72,73 @@ export class createExperience extends React.Component {
 							<label htmlFor="stickyness">Stickyness</label>
 						</div>
 						<div className="col-25">
-							<input type="text" id="stickyness" name="stickyness"  />
+							<input type="text" id="stickyness" name="stickyness" />
 						</div>
-						<div className="col-25">
+						<div className="col-1">
 							<label htmlFor="stickyness">minutes</label>
 						</div>
 					</div>
 
 					<div className="row">
 						<div className="col-25">
-							<label htmlFor="stickyness">Experiment Duration</label>
+							<label htmlFor="duration">Experiment Duration</label>
 						</div>
 						<div className="col-25">
-							<input type="text" id="duration" name="duration"  />
+							<input type="text" id="duration" name="duration" />
 						</div>
-						<div className="col-25">
-							<label htmlFor="stickyness">minutes</label>
+						<div className="col-1">
+							<label htmlFor="duration">minutes</label>
 						</div>
 					</div>
 
 					<div className="row">
-						<input type="submit" value="Submit"  />
+						<div className="col-sm-12">
+							<input type="submit" value="Submit" data-toggle="modal" data-target="#myModal" />
+						</div>
 					</div>
 				</form>
+
+
+				<div className="modal fade" id="myModal" role="dialog">
+					<div className="modal-dialog">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h4 className="modal-title">Summary</h4>
+							</div>
+							<div className="modal-body">
+								<div className="leftAllign">
+									<span>Experiment name:</span>
+									<span>{this.state.formObj.experimentName}</span>
+								</div>
+								<div className="leftAllign">
+									<span>End point:</span>
+									<span>API.tesco.com/AB experiment/{this.state.formObj.experimentName}</span>
+								</div>
+								<div className="leftAllign">
+									<span>Request body:</span>
+									<div className="reqBody">
+										<span>{"{"}</span>
+										<span className="dispFlex">Experiment name: {this.state.formObj.experimentName}</span>
+										<span className="dispFlex">Percentage: {this.state.formObj.percentage}</span>
+										<span className="dispFlex">Stickyness: {this.state.formObj.stickyness}</span>
+										<span className="dispFlex">Duration: {this.state.formObj.duration}</span>
+										<span>{"}"}</span>
+									</div>
+								</div>
+
+							</div>
+							<div className="modal-footer">
+								<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
 			</div>
+
+
+
 
 		)
 
